88afa1be263079f410116568e5aa0a59dec844f8
Michael Borisov <michael.borisov@web.de>
2023-11-17 16:05:07 +0100

    deleted combined growing and sampling
    the algorith idea was deleted again, because its not needed anymore (lloyd algorithm fixed the issue and was easier to implement)

--- src/meshlets/clustering/CombinedGrowingAndSampling.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../Meshlets.h"
-
-namespace meshlets {
-std::vector<Site> combined_growing_and_sampling(pmp::SurfaceMesh &mesh,
-                                                int max_iterations = 1000);
-} // namespace meshlets
